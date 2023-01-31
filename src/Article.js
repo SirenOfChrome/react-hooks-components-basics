9ae277357ecbb7d424f2722e407d727e2ac6c100
Forrest Jones <forrestjones2010@gmail.com>
2023-01-31 11:13:20 -0600

    DGC#3

--- src/Article.js
+++ src/Article.js
@@ -5,5 +5,10 @@ function Article() {
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
   );
 }
+function Comment() {
+  return <div> Naturally, I agree with this article.</div>
+}
 
 export default Article;
+
+//The minimum requirement for a React component is that it must be a function that starts with a capital letter and returns JSX.
